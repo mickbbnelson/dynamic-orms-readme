63a75fde8151794bafc32da716a1734678e1101d
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-05-03 16:24:31 -0700

    done

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,17 @@
 require 'sqlite3'
 
 
-DB = {:conn => SQLite3::Database.new("db/songs.db")}
-DB[:conn].execute("DROP TABLE IF EXISTS songs")
+DB = {:conn => SQLite3::Database.new("db/songs.db")}   #Creates the database
+DB[:conn].execute("DROP TABLE IF EXISTS songs")   #Drops any existing songs table if there happens to be one
 
-sql = <<-SQL
+sql = <<-SQL                                
   CREATE TABLE IF NOT EXISTS songs (
   id INTEGER PRIMARY KEY,
   name TEXT,
   album TEXT
   )
 SQL
-
+                                        #^Creates table in database
 DB[:conn].execute(sql)
-DB[:conn].results_as_hash = true
+DB[:conn].results_as_hash = true        #results_as_hash is a method provided to us by the sqlite3-ruby-gem.
+                                        #This method says: when a SELECT statement is executed, don't return a database row as an array, return it as a hash with the column names as keys.
--- lib/song.rb
+++ lib/song.rb
@@ -4,28 +4,28 @@ require 'active_support/inflector'
 class Song
 
 
-  def self.table_name
-    self.to_s.downcase.pluralize
+  def self.table_name               #takes the name of the class, referenced by the self keyword, turns it to a string, downcases and pluralizes
+    self.to_s.downcase.pluralize    #gem needed for the pluralize method
   end
 
   def self.column_names
-    DB[:conn].results_as_hash = true
+    DB[:conn].results_as_hash = true    #This method says: when a SELECT statement is executed, don't return a database row as an array, return it as a hash with the column names as keys.
 
-    sql = "pragma table_info('#{table_name}')"
+    sql = "pragma table_info('#{table_name}')"      #querys a table for the names of its columns
 
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |row|
-      column_names << row["name"]
-    end
+      column_names << row["name"]     #We iterate over the resulting array of hashes to collect just the name of each column.
+    end                                #The return value of calling Song.column_names will therefore be:["id", "name", "album"]
     column_names.compact
   end
 
   self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
+    attr_accessor col_name.to_sym         #tells the class that it should have attr_accessors named after each column name
   end
 
-  def initialize(options={})
+  def initialize(options={})    #We iterate over the options hash and use our fancy metaprogramming #send method to interpolate the name of each hash key as a method that we set equal to that key's value.
     options.each do |property, value|
       self.send("#{property}=", value)
     end
@@ -38,10 +38,10 @@ class Song
   end
 
   def table_name_for_insert
-    self.class.table_name
+    self.class.table_name       #access the table name we want to INSERT into from inside our #save method,
   end
 
-  def values_for_insert
+  def values_for_insert         #iterate over the column names stored in #column_names and use the #send method with each individual column name to invoke the method by that same name and capture the return value:
     values = []
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
@@ -50,10 +50,10 @@ class Song
   end
 
   def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")  #Delets the column name if it = id
   end
 
-  def self.find_by_name(name)
+  def self.find_by_name(name)  #it uses the #table_name class method we built that will return the table name associated with any given class.
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
